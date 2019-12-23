246e1bee9439f4baae82e997e3c110bef075ce23
Nemanja <nemanjane24@gmail.com>
2019-12-23 08:15:00 +0100

    Updated props and css

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import style from './Heading.module.css'; 
 import star from '../../img/logo.svg';
-import santa from '../../img/santa1.png'
 
 const Heading = () => {
+  const value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et pellentesque elit. Pellentesque non mattis sapien. Ut est velit, commodo.";
+  
   return(
-    <div>
-      <h1 className={style.secret_santa}>-Secret Santa-</h1>
+    <div className={style.santa}>
+      <h1 className={style.santa_headline}>-Secret Santa-</h1>
       <img src={star} className={style.image_star} alt='star' />
-      <p className={style.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et pellentesque elit. Pellentesque non mattis sapien. Ut est velit, commodo.</p>
-      <img src={santa} className={style.image_santa} alt='santa' />
+      <p className={style.santa_text}>{value}</p>
     </div>
   );
 }
