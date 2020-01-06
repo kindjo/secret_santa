136123cd9261346bf4cad0d3b6752fc91db25467
Nemanja <nemanjane24@gmail.com>
2020-01-06 14:09:58 +0100

    Added renders to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import style from './App.module.css';
+import Heading from './components/Heading/Heading';
+import Participants from './components/Participants/Participants';
+import santa from './img/santa.svg';
+
 function App() {
   return (
     <div>
+      <Heading />
+      <img className={style.image_santa} src={santa} alt="Santa" />
+      <Participants />
     </div>
   );
 }
